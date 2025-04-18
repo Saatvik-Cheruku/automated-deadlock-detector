8227b1284291019f17aa14b7dccd6f75ee422909
Saatvik Cheruku <saatvikcheruku@email.com>
2025-04-18 14:35:40 +0530

    Add: Basic package structure with __init__.py

---
+++ gui/__init__.py
@@ -0,0 +1,6 @@
+"""
+Deadlock Detector GUI Package
+
+This package provides a graphical interface for simulating and detecting deadlocks
+in operating systems using Resource Allocation Graphs (RAG).
+""" 
